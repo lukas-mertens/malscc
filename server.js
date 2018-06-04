3919b83ae796882e07ba315bfc86f6223b1b89eb
Lukas Mertens <info@lukas-mertens.de>
2018-06-04 20:53:41 +0200

    JavaScript style improvements
    Made improvements based on codacy-review

--- server.js
+++ server.js
@@ -1,25 +1,25 @@
 require("babel-polyfill");
 
-var express = require('express')
-  , logger = require('morgan')
+var express = require("express")
+  , logger = require("morgan")
   , app = express()
-  , template = require('jade').compileFile(__dirname + '/source/templates/index.jade');
+  , template = require("jade").compileFile(__dirname + "/source/templates/index.jade");
 
-var $ = require('jquery');
+var $ = require("jquery");
 
 
-app.use(logger('dev'))
-app.use(express.static(__dirname + '/static'))
+app.use(logger("dev"));
+app.use(express.static(__dirname + "/static"));
 
-app.get('/', function (req, res, next) {
+app.get("/", function (req, res, next) {
   try {
-    var html = template({ title: 'index' })
-    res.send(html)
+    var html = template({ title: "index" });
+    res.send(html);
   } catch (e) {
     next(e)
   }
-})
+});
 
 app.listen(process.env.PORT || 3000, function () {
-  console.log('Listening on http://localhost:' + (process.env.PORT || 3000))
+  console.log("Listening on http://localhost:" + (process.env.PORT || 3000));
 })
